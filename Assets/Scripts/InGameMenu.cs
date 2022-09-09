98e1ce1d6ef89981aa2cdab09e188e71169da11f
MingbinY <Mingbin.Yang@outlook.com>
2022-09-09 16:44:09 -0400

    in game menu

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenuButton()
+    {
+
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
